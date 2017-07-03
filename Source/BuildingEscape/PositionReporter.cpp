cf45f2954bd313cdef7cacff696ed269c55fa387
Shadow pryo <terry.dewayne.hicks@gmail.com>
2017-07-02 22:25:09 -0400

    Updated UE and added raycast

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -3,6 +3,7 @@
 #include "BuildingEscape.h"
 #include "PositionReporter.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
@@ -33,6 +34,52 @@ void UPositionReporter::TickComponent( float DeltaTime, ELevelTick TickType, FAc
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
+	// Get player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	/* Draw a red trace in the world to visual
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);*/
+	
+	/// Setup quary 
+	FCollisionQueryParams TraceParameters(FName(TEXT("")),
+		false,
+		GetOwner()
+	);
+
+
+	/// Line Trace (aka Ray-cast) out to reach distance
+	FHitResult Hit;
+	
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	/// See what we hit
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit) {
+		UE_LOG(LogTemp, Warning, TEXT("Object name is %s"), (*ActorHit->GetName()));
+	}
+
 }
 
--- Source/BuildingEscape/PositionReporter.h
+++ Source/BuildingEscape/PositionReporter.h
@@ -19,8 +19,9 @@ public:
 	virtual void BeginPlay() override;
 	
 	// Called every frame
-	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;	
 
-		
+private:
+	float Reach = 100.f;
 	
 };
